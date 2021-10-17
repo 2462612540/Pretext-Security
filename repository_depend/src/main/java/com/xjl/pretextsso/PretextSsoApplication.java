1ffc0220fa36246ad2a77d7815d0975615628fbb
xjl <18279148786@163.com>
2021-10-17 10:01:46 +0800

    feature(pretext-sso)[xjl-001]: init project and test push remote git repo

---
+++ repository_depend/src/main/java/com/xjl/pretextsso/PretextSsoApplication.java
@@ -0,0 +1,13 @@
+package com.xjl.pretextsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretextSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PretextSsoApplication.class, args);
+    }
+
+}
---
+++ repository_depend/src/test/java/com/xjl/pretextsso/PretextSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjl.pretextsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretextSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
